eddd7f856c7dc423c387d496a87cf5fdf941215b
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2014-08-18 23:16:58 -0400

    New: Add value to VisitedThreadSet or check if it exists

--- helpers/visited_thread_set.py
+++ helpers/visited_thread_set.py
@@ -4,16 +4,19 @@ class VisitedThreadSet():
     set = None
 
     def __init__(self):
+        self.set = set()
+
+    def load_set(self):
         pass
 
-    def load(self):
+    def save_set(self):
         pass
 
-    def save(self):
-        pass
+    def add(self, value):
+        self.set.add(str(value))
 
-    def add_thread(self):
-        pass
-
-    def check_thread_exists(self):
-        pass
+    def contains(self, value):
+       if str(value) in self.set:
+           return True
+       else:
+           return False
