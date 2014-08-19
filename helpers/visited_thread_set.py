4630ba2160646d0091f1ad7e74f94773bb8dfd66
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2014-08-18 23:55:03 -0400

    New: Visited threads get loaded and saved

--- helpers/visited_thread_set.py
+++ helpers/visited_thread_set.py
@@ -1,16 +1,24 @@
+import json
 
 
 class VisitedThreadSet():
+    file_name = "visited_threads.txt"
     set = None
 
     def __init__(self):
         self.set = set()
+        self.load_set()
 
     def load_set(self):
-        pass
+        file = open(self.file_name, 'r')
+        name_array = json.load(file)
+        self.set = set(name_array)
+        print self.set
 
     def save_set(self):
-        pass
+        file = open(self.file_name, 'w')
+        json.dump(list(self.set), file)
+        file.close()
 
     def add(self, value):
         self.set.add(str(value))
