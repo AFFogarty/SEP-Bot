da8680ac10ff13ab3a2eef59621bfd84316d4021
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2014-08-19 00:18:44 -0400

    New: First iteration of SEP-Bot

---
+++ sep_bot.py
@@ -0,0 +1,49 @@
+from helpers.visited_thread_set import VisitedThreadSet
+from helpers.post_formatter import PostFormatter
+from praw.errors import APIException
+from sep.sep_search_result import SEPSearchResult
+import praw
+import authentication
+import logging
+
+# Logging
+logging.basicConfig(filename="sep-bot.log",
+                    level=logging.INFO,
+                    format='%(asctime)s %(message)s')
+
+r = praw.Reddit('SEP-Bot by /u/StealthSilver')
+r.login(authentication.username, authentication.password)
+
+# The subreddit used by the sep bot
+sep_bot_subreddit = r.get_subreddit("SEPBot")
+test_thread = r.get_submission(submission_id="2dwcwi")
+formatter = PostFormatter()
+search = None
+
+already_done = VisitedThreadSet()
+
+subreddit = r.get_subreddit('askphilosophy')
+for submission in subreddit.get_hot(limit=300):
+    op_text = submission.selftext.lower()
+    # Test if it contains a PRAW-related question
+    if not already_done.contains(submission.id):
+        print "\n\n-----------------------------------\n\n"
+        title = u"{0}".format(submission.title)
+        print "\n{0}\n".format(title)
+        search = SEPSearchResult(title)
+        results = search.request_results()
+
+        new_post = formatter.relevant_articles_post(results)
+
+        # test_thread.add_comment(new_post)
+        # try:
+        submission.add_comment(new_post)
+        already_done.add(submission.id)
+        already_done.save_set()
+        # except APIException:
+        #     error_message = "Error on submission {0}: {1}".format(submission.id, APIException)
+        #     print error_message
+        #     logging.info(error_message)
+    # time.sleep(1800)
+
+already_done.save_set()
