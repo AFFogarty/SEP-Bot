d5af806dcb4c8921f63f4bc6c58840b2da6bed95
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2014-08-18 22:54:30 -0400

    New: Class to encapsulate the "VisitedThread" file

---
+++ visited_thread_set.py
@@ -0,0 +1,19 @@
+
+
+class VisitedThreadSet():
+    set = None
+
+    def __init__(self):
+        pass
+
+    def load(self):
+        pass
+
+    def save(self):
+        pass
+
+    def add_thread(self):
+        pass
+
+    def check_thread_exists(self):
+        pass
